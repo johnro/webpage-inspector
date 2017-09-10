625c2c7a8794b75805a0f6b444b8d7426bcc78dc
John Rongved <john.rongved@gmail.com>
2017-09-10 12:25:02 +0200

    added Api connector, enable post request

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,22 @@
-console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}, your user was registered!`
+  })
+})
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Hello world!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
 
