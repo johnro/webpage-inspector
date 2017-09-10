720316ab1e4bb2ff5202dd6028f28f50ee703764
John Rongved <john.rongved@gmail.com>
2017-09-10 12:54:36 +0200

    added Register component, enable access server

---
+++ client/src/components/Register.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>register</h1>
+
+    <input
+      type="text"
+      name="email"
+      v-model="email"
+      placeholder="email"/>
+    <br>
+    <button
+      @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+  import AuthenticationService from '@/services/AuthenticationService'
+  export default {
+    data () {
+      return {
+        email: 'emaisdsds'
+      }
+    },
+    methods: {
+      async register () {
+        const response = await AuthenticationService.register({
+          email: this.email
+        })
+        console.log(response.data)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.post('/register', (req, res) => {
   res.send({
